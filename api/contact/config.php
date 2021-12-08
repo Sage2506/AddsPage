6475423c6fb361506bf04a5178d592881ebedeb9
Eduardo Salazar <german2506.gs@gmail.com>
2021-12-08 11:40:02 -0700

    las config index and sendmail

--- api/contact/config.php
+++ api/contact/config.php
@@ -1,5 +1,5 @@
 <?php
 $adminEmail =  "info@impactovisual.info"; //Please change this to yours
-//$adminEmail =  "german2506.gs@gmail.com"; //Please change this to yours
+//$adminEmail =  "german_sjj4@hotmail.com"; //Please change this to yours
 $SendMailFailederrorMessage = "Something went wrong";
 $SendMailEmptyerrorMessage = "Empty form";
